7cc105bc64e5ed59ae11b260a6d1f635004c048b
0xe20x8d0xbc <0xe20x8d0xbc@example.com>
2023-07-09 00:56:21 +0200

    Added readme and long description

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='SudReg',
     version='1.0.0',
@@ -7,6 +11,8 @@ setup(
     author='Your Name',
     author_email='0xe20x8d0xbc@gmail.com',
     description='A light weight Python library for the SudReg Web API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'requests'
